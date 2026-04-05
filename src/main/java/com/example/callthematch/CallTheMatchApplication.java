d58b4e113ca9246c39a5c4085fd46fbf7402e4d9
Edis <edis.hurtic@student.hogent.be>
2026-04-05 18:37:10 +0200

    Adding error pages

--- src/main/java/com/example/callthematch/CallTheMatchApplication.java
+++ src/main/java/com/example/callthematch/CallTheMatchApplication.java
@@ -2,12 +2,19 @@ package com.example.callthematch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CallTheMatchApplication {
+public class CallTheMatchApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(CallTheMatchApplication.class, args);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/home");
     }
 
 }
